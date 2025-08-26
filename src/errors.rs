75f6ba69820c8a99c01a0dc9a5471b565ce3b2e3
Paweł Gorgoń <pgorgon@hem-e.com>
2025-08-26 11:59:26 +0200

    Changed log severity for UnexpectedEof to debug, as it is an error caused by normal operation, not actual error

--- src/errors.rs
+++ src/errors.rs
@@ -50,7 +50,11 @@ impl error::Error for DecodeError {
 
 impl From<io::Error> for DecodeError {
     fn from(err: io::Error) -> DecodeError {
-        log::error!("Got I/O error {err}");
+        if err.kind() != io::ErrorKind::UnexpectedEof {
+            log::error!("Got I/O error: {err}");
+        } else {
+            log::debug!("Got I/O error: {err}");
+        }
         match err.kind() {
             io::ErrorKind::UnexpectedEof => DecodeError::Truncated(err),
             io::ErrorKind::Other => {
